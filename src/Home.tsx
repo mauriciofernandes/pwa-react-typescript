1fb7246778a4a2e4281d81196ea127194255e316
Mauricio Fernandes <mauriciodiasf@gmail.com>
2020-06-08 00:02:58 +0200

    test

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Home: React.FC = () => (
-  <h1>Welcome to your PWA!</h1>
+  <>
+    <h1>Welcome to your PWA!</h1>
+    <a href="intent://send/0123456789#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">OPEN WHATSAPP ANDROID</a>
+  </>
 );
 
 export default Home;
