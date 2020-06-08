5496d09452fa5d275aee166ac05f667df5bfd765
mauriciofernandes <mauriciodiasf@gmail.com>
2020-06-08 13:04:28 +0200

    test whatsapp

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home: React.FC = () => (
   <>
     <h1>Welcome to your PWA!</h1>
-    <a href="intent://send/0123456789#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">OPEN WHATSAPP ANDROID</a>
+    <a href="https://api.whatsapp.com/send?phone=5581986987147">OPEN WHATSAPP</a>
   </>
 );
 
