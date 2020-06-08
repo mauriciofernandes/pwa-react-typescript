fdbe92627440b1fa6bb3122ae569023782cf1dac
mauriciofernandes <mauriciodiasf@gmail.com>
2020-06-08 13:28:54 +0200

    update phone

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home: React.FC = () => (
   <>
     <h1>Welcome to your PWA!</h1>
-    <a href="https://api.whatsapp.com/send?phone=5581986987147">OPEN WHATSAPP</a>
+    <a href="https://api.whatsapp.com/send?phone=0000000000000">OPEN WHATSAPP</a>
   </>
 );
 
